f9738a8a5db7883a3ea10ca6e5f07882a510daf0
Bhuvaneshwaran <bhuvaneshwaran319@gmail.com>
2024-08-21 10:11:18 +0530

    Implemented copy to clipboard

--- composeApp/src/androidMain/kotlin/com/acutecoder/irchat/presentation/Extensions.android.kt
+++ composeApp/src/androidMain/kotlin/com/acutecoder/irchat/presentation/Extensions.android.kt
@@ -1,5 +1,7 @@
 package com.acutecoder.irchat.presentation
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.database.Cursor
 import android.graphics.BitmapFactory
@@ -8,10 +10,18 @@ import android.provider.OpenableColumns
 import android.util.Log
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
-import java.io.InputStream
+import com.acutecoder.irchat.core.InputStream
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-actual fun logInternal(tag: String, message: String) {
-    Log.e(tag, message)
+actual fun Any?.logInternal(message: Any?) {
+    val stackTrace = Thread.currentThread().stackTrace[2]
+    Log.e(
+        if (this is String) this else "$this",
+        "$message (${stackTrace.fileName}:${stackTrace.lineNumber})"
+    )
 }
 
 actual fun InputStream.loadAsImageBitmap(): ImageBitmap {
@@ -32,4 +42,20 @@ fun Context.getFileName(uri: Uri): String? {
     }
 
     return fileName
+}
+
+actual fun copyToClipboard(text: String) {
+    val context: Context = injectInstance()
+    val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    clipboardManager.setPrimaryClip(
+        ClipData("Copied text", arrayOf("text/plain"), ClipData.Item(text))
+    )
+}
+
+actual suspend inline fun <T> withIO(noinline block: suspend CoroutineScope.() -> T): T {
+    return withContext(Dispatchers.IO, block)
+}
+
+actual fun CoroutineScope.launchIO(block: suspend CoroutineScope.() -> Unit) {
+    launch(Dispatchers.IO, block = block)
 }
--- composeApp/src/desktopMain/kotlin/com/acutecoder/irchat/presentation/Extensions.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/acutecoder/irchat/presentation/Extensions.desktop.kt
@@ -2,12 +2,36 @@ package com.acutecoder.irchat.presentation
 
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.loadImageBitmap
-import java.io.InputStream
+import com.acutecoder.irchat.core.InputStream
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
 
-actual fun logInternal(tag: String, message: String) {
-    System.err.println("$tag: $message")
+actual fun Any?.logInternal(message: Any?) {
+    val stackTrace = Thread.currentThread().stackTrace[2]
+    System.err.println(
+        if (this is String) this else "$this" +
+                ": $message (${stackTrace.fileName}:${stackTrace.lineNumber})"
+    )
 }
 
 actual fun InputStream.loadAsImageBitmap(): ImageBitmap {
     return loadImageBitmap(this)
+}
+
+actual fun copyToClipboard(text: String) {
+    val stringSelection = StringSelection(text)
+    val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+    clipboard.setContents(stringSelection, null)
+}
+
+actual suspend inline fun <T> withIO(noinline block: suspend CoroutineScope.() -> T): T {
+    return withContext(Dispatchers.IO, block)
+}
+
+actual fun CoroutineScope.launchIO(block: suspend CoroutineScope.() -> Unit) {
+    launch(Dispatchers.IO, block = block)
 }
