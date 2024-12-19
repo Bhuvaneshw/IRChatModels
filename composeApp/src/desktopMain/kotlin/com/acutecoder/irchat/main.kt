47a75ee1231f7087d780120732dcc16e9fc6e810
thami <thamilthennarasu@gmail.com>
2024-12-19 21:50:12 +0530

    Set minimum window size for desktop

--- composeApp/src/desktopMain/kotlin/com/acutecoder/irchat/main.kt
+++ composeApp/src/desktopMain/kotlin/com/acutecoder/irchat/main.kt
@@ -3,8 +3,10 @@ package com.acutecoder.irchat
 import androidx.compose.ui.window.ApplicationScope
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.acutecoder.irchat.di.initKoin
 import com.acutecoder.irchat.presentation.App
+import java.awt.Dimension
 
 fun main() {
     initKoin()
@@ -15,7 +17,9 @@ fun main() {
         Window(
             onCloseRequest = ::exitApplication,
             title = "IR Chat Models",
+            state = rememberWindowState()
         ) {
+            window.minimumSize = Dimension(400, 550)
             App()
         }
     }
